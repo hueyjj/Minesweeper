6a8adececb241a9a9d0c542f2a753c3b26af03fa
unknown <jasperjeng@gmail.com>
2019-05-18 21:39:46 -0700

    Added Grid

--- src/com/company/Grid.java
+++ src/com/company/Grid.java
@@ -4,21 +4,52 @@ import java.util.Random;
 
 public class Grid {
     private int width, height;
+    private int numBombs;
     private int[][] grid;
-    public Grid(int width, int height){
+
+    public Grid(int width, int height, int numBombs){
         this.width = width;
         this.height = height;
+        this.numBombs = numBombs;
         grid = new int[height][width];
+        this.initGrid(this.numBombs);
     }
 
     public void initGrid(int numBombs) {
-        Random random = new Random();
-        // Clear grid and set bombs
+        // Clear grid
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                if (random.nextInt(numBombs))
-                grid[row][col] = -1;
+                grid[row][col] = 0;
             }
         }
+
+        // Set bombs
+        Random random = new Random();
+        int n = numBombs;
+        while (n > 0) {
+            int row = random.nextInt(this.height);
+            int col = random.nextInt(this.width);
+            // Bomb doesn't already exists
+            if (this.grid[row][col] != -1) {
+                this.grid[row][col] = -1;
+                n--;
+            }
+        }
+    }
+
+    public int[][] get2DArray() {
+        return this.grid;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public int getNumBombs() {
+        return this.numBombs;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Grid grid = new Grid(10, 10, 25);
+        int[][] gridArr = grid.get2DArray();
+        for (int i = 0; i < grid.getHeight(); i++) {
+            for (int j = 0; j < grid.getWidth(); j++) {
+                System.out.printf("%2d ", gridArr[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
