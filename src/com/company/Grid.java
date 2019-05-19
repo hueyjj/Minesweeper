068a67c677a5b8a422b47173acf2babcc54f0362
unknown <jasperjeng@gmail.com>
2019-05-19 02:56:38 -0700

    Working version

--- src/com/company/Grid.java
+++ src/com/company/Grid.java
@@ -1,15 +1,19 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 
 public class Grid {
     private int width, height;
     private int numBombs;
+    private boolean hasLost;
 
     /*
-        0: unmarked
+        0: un-open
         -1: bomb
-        -2: question mark
+        -2: marked a bomb
+        -4: marked not a bomb
         -3: opened
         > 0: num of bombs adjacent
      */
@@ -19,6 +23,7 @@ public class Grid {
         this.width = width;
         this.height = height;
         this.numBombs = numBombs;
+        this.hasLost = false;
         grid = new int[height][width];
         this.initGrid(this.numBombs);
     }
@@ -65,9 +70,116 @@ public class Grid {
         return this.numBombs;
     }
 
-    public void setQuestionMark(int row, int col) {
-        if (0 <= row && row < this.height && 0 <= col && col <= this.width) {
-            this.grid[row][col] = -2;
+    private class Location {
+        public int row, col;
+        public Location(int row, int col){
+            this.row = row;
+            this.col = col;
         }
+    }
+
+    public void setOpen(int row, int col) {
+        if (0 <= row && row < this.height && 0 <= col && col <= this.width) {
+            // Hit a bomb, we lose
+            if (this.grid[row][col] == -1) {
+                this.hasLost = true;
+                return;
+            }
+
+            // Open as much area as possible
+            Queue<Location> queue = new LinkedList<>();
+            queue.add(new Location(row, col));
+            while (queue.size() > 0) {
+                Location loc = queue.remove();
+                //System.out.printf("Removed: (%2d, %2d)\n", loc.row, loc.col);
+                int numAdjacentBombs = 0;
+                boolean clear = true;
+                // Check top left
+                if (loc.col-1 >= 0 && loc.row-1 >= 0
+                        && (this.grid[loc.row-1][loc.col-1] == -1 || this.grid[loc.row-1][loc.col-1] == -2)) {
+                    clear = false;
+                    numAdjacentBombs++;
+                }
+                // Check middle left
+                if (loc.col-1 >= 0
+                        && (this.grid[loc.row][loc.col-1] == -1 || this.grid[loc.row][loc.col-1] == -2)) {
+                    clear = false;
+                    numAdjacentBombs++;
+                }
+                // Check bottom left
+                if (loc.col-1 >= 0 && loc.row+1 < this.height
+                        && (this.grid[loc.row+1][loc.col-1] == -1 || this.grid[loc.row+1][loc.col-1] == -2)) {
+                    clear = false;
+                    numAdjacentBombs++;
+                }
+                // Check middle top
+                if (loc.row-1 >= 0
+                        && (this.grid[loc.row-1][loc.col] == -1 || this.grid[loc.row-1][loc.col] == -2)) {
+                    clear = false;
+                    numAdjacentBombs++;
+                }
+                // Check middle bottom
+                if (loc.row+1 < this.height
+                        && (this.grid[loc.row+1][loc.col] == -1 || this.grid[loc.row+1][loc.col] == -2)) {
+                    clear = false;
+                    numAdjacentBombs++;
+                }
+                // Check top right
+                if (loc.row-1 >= 0 && loc.col+1 < this.width
+                        && (this.grid[loc.row-1][loc.col+1] == -1 || this.grid[loc.row-1][loc.col+1] == -2)) {
+                    clear = false;
+                    numAdjacentBombs++;
+                }
+                // Check middle right
+                if (loc.col+1 < this.width
+                        && (this.grid[loc.row][loc.col+1] == -1 || this.grid[loc.row][loc.col+1] == -2)) {
+                    clear = false;
+                    numAdjacentBombs++;
+                }
+                // Check bottom right
+                if (loc.col+1 < this.width && loc.row+1 < this.height
+                        && (this.grid[loc.row+1][loc.col+1] == -1 || this.grid[loc.row+1][loc.col+1] == -2)) {
+                    clear = false;
+                    numAdjacentBombs++;
+                }
+
+                if (clear) {
+                    if (loc.row-1 >= 0 && loc.col-1 >= 0 && this.grid[loc.row-1][loc.col-1] == 0)
+                        queue.add(new Location(loc.row - 1, loc.col - 1));
+                    if (loc.col-1 >= 0 && this.grid[loc.row][loc.col-1] == 0)
+                        queue.add(new Location(loc.row, loc.col-1));
+                    if (loc.row+1 < this.height && loc.col-1 >= 0 && this.grid[loc.row+1][col-1] == 0)
+                        queue.add(new Location(loc.row+1, loc.col-1));
+                    if (loc.row-1 >= 0 && this.grid[loc.row-1][loc.col] == 0)
+                        queue.add(new Location(loc.row-1, loc.col));
+                    if (loc.row+1 < this.height && this.grid[loc.row+1][loc.col] == 0)
+                        queue.add(new Location(loc.row+1, loc.col));
+                    if (loc.row-1 >= 0 && loc.col+1 < this.width && this.grid[loc.row-1][loc.col+1] == 0)
+                        queue.add(new Location(loc.row-1, loc.col+1));
+                    if (loc.col+1 < this.width && this.grid[loc.row][loc.col+1] == 0)
+                        queue.add(new Location(loc.row, loc.col+1));
+                    if (loc.row+1 < this.height && loc.col+1 < this.width && this.grid[loc.row+1][loc.col+1] == 0)
+                        queue.add(new Location(loc.row+1, loc.col+1));
+
+                    this.grid[loc.row][loc.col] = -3;
+                } else {
+                    this.grid[loc.row][loc.col] = numAdjacentBombs;
+                }
+            }
+        }
+    }
+
+    public void setMark(int row, int col) {
+        if (0 <= row && row < this.height && 0 <= col && col < this.width) {
+            if (this.grid[row][col] == -1) {
+                this.grid[row][col] = -2; // Found a bomb and marked
+            } else {
+                this.grid[row][col] = -3; // Didn't find a bomb, but marking anyway...
+            }
+        }
+    }
+
+    public boolean hasLost() {
+        return this.hasLost;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,37 +5,78 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Grid minesweeper = new Grid(10, 10, 25);
+        Grid minesweeper = new Grid(10, 10, 10);
         int[][] grid = minesweeper.get2DArray();
+
+        printNakedGrid(grid, minesweeper.getWidth(), minesweeper.getHeight());
+        System.out.println();
+
         Scanner scanner = new Scanner(System.in);
         String input;
         boolean running = true;
         while (running) {
-            System.out.print("Input: ");
+            printGrid(grid, minesweeper.getWidth(), minesweeper.getHeight());
+            System.out.print("Command (open (o), mark (m), quit (q), help (h)): ");
             input = scanner.next();
             switch(input) {
+                case "o":
+                case "open": {
+                    System.out.print("Row: ");
+                    int row = scanner.nextInt();
+                    System.out.print("Column: ");
+                    int col = scanner.nextInt();
+                    minesweeper.setOpen(row, col);
+                    if (minesweeper.hasLost()) {
+                        running = false;
+                        continue;
+                    }
+                    break;
+                }
+                case "m":
+                case "mark": {
+                    System.out.print("Row: ");
+                    int row = scanner.nextInt();
+                    System.out.print("Column: ");
+                    int col = scanner.nextInt();
+                    minesweeper.setMark(row, col);
+                    break;
+                }
+                case "q":
                 case "quit":
                     System.out.println();
-                    System.out.println("Exit minesweeper.");
-                    running = true;
+                    System.out.println("Bye. minesweeper finished.");
+                    running = false;
                     continue;
+                case "h":
+                case "help":
+                    System.out.println("Example:");
+                    System.out.println("    Enter 'o', enter a row index, enter a column index");
+                    System.out.println();
+                    break;
                 default:
+                    System.out.println("Unrecognized command.");
+                    System.out.println();
                     break;
             }
         }
     }
 
     public static void printGrid(int[][] grid, int width, int height) {
+        // Column header
+        System.out.printf("%2s ", " ");
+        for (int col = 0; col < width; col++)
+            System.out.printf("%2s ", col);
+        System.out.println();
+
         for (int row = 0; row < height; row++) {
+            System.out.printf("%2s ", row);
             for (int col = 0; col < width; col++) {
                 String out = "";
                 int value = grid[row][col];
                 switch(value) {
                     case 0:
-                        out = ".";
-                        break;
                     case -1:
-                        out = "X";
+                        out = ".";
                         break;
                     case -2:
                         out = "?";
@@ -44,9 +85,26 @@ public class Main {
                         out = " ";
                         break;
                     default:
+                        out = grid[row][col] + "";
                         break;
                 }
                 System.out.printf("%2s ", out);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printNakedGrid(int[][] grid, int width, int height) {
+        // Column header
+        System.out.printf("%2s ", " ");
+        for (int col = 0; col < width; col++)
+            System.out.printf("%2s ", col);
+        System.out.println();
+
+        for (int row = 0; row < height; row++) {
+            System.out.printf("%2s ", row);
+            for (int col = 0; col < width; col++) {
+                System.out.printf("%2s ", grid[row][col]);
             }
             System.out.println();
         }
